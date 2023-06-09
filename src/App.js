2a5a765921f1113f83026d7dcfc47b8c3a9d28ef
lazychino <lazy.chino@gmail.com>
2023-06-09 16:36:56 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from "react";
-import { useFetch } from "@uidotdev/usehooks";
-import "./style.css";
+import React from 'react';
+import { useFetch } from '@uidotdev/usehooks';
+import './style.css';
 
 const API_KEY = window.localStorage.getItem('airnow');
 
 export default function App() {
-  
-  
   const { error, data } = useFetch(
     `https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=07306&date=2023-06-09&distance=25&API_KEY=${API_KEY}`
   );
@@ -15,6 +13,7 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <pre>{data}</pre>
     </div>
   );
 }
