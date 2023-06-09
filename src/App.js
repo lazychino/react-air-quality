3173721cf7be46e1868b6c7f6c751536d3bc8311
lazychino <lazy.chino@gmail.com>
2023-06-09 17:07:59 -0400

    api working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useFetch } from '@uidotdev/usehooks';
 import './style.css';
 
 const API_KEY = window.localStorage.getItem('airnow');
 
+const getAirQualityData = (zip) =>
+  fetch(
+    `https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=${zip}&date=2023-06-09&distance=25&API_KEY=${API_KEY}`
+  ).then((response) => response.json());
+
 export default function App() {
-  const { error, data } = useFetch(
-    `https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=07306&date=2023-06-09&distance=25&API_KEY=${API_KEY}`
-  );
+  const [zipcode, setZipCode] = useState('10001');
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    getAirQualityData(zipcode).then((response) => {
+      setData(JSON.stringify(response));
+      console.log(response);
+    });
+
+    return () => {};
+  }, [zipcode]);
 
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      
       <pre>{data}</pre>
     </div>
   );
